2779a60c3aa9afb5c669eed3c1678375cec8a508
Ignacio <ignasiamat10@gmail.com>
2024-01-23 11:40:40 +0100

    livewire config components updated, web routes updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Livewire\Tasks\TaskShow;
+use App\Livewire\Tasks\TaskCreate;
+use App\Livewire\Tasks\TaskIndex;
+use App\Livewire\Tasks\TaskUpdate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', TaskIndex::class)->name('tasks.index');
+Route::get('/tasks/create', TaskCreate::class)->name('tasks.create');
+Route::get('/tasks/update', TaskUpdate::class)->name('tasks.update');
+Route::get('/tasks/{task}', TaskShow::class)->name('tasks.show');
